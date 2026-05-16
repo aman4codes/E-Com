ea1706b6ce7794a4e8d545dbe13013d16ff91467
aman4codes <aman4codes@gmail.com>
2026-05-17 00:11:51 +0530

    Intial Commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Backend is working")
+
+})
+
+app.listen(5001, () => {
+    console.log(`Server is running on port 5001`);
+})
