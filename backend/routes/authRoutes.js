3a9e97ec2f88897ed5091be75e6a4ea3f9edd7d9
aman4codes <aman4codes@gmail.com>
2026-05-21 23:49:26 +0530

    Finished

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { userSignUp, LoginUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/auth/signup", userSignUp);
-router.post("/auth/login", LoginUser);
-
-export default router;
--- frontend/src/pages/ProductDetails.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProductDetails() {
-  return (
-    <div>
-      <h1>Welcome to ProductDetails page</h1>
-    </div>
-  );
-}
